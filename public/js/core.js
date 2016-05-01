1d2ee36cbc2e1a4ec8dea97dfe8b6bfe48357ffa
Leonab <leonabrocks@yahoo.com>
2016-05-01 15:27:06 +0530

    Switched to Angular-Material

--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,1 @@
-angular.module('leonab', ['locationController', 'locationService','modalFormApp','ngAnimate','ui.bootstrap','ui.router','ngRoute','vAccordion']);
+angular.module('leonab', ['locationController', 'locationService','angular-loading-bar','modalFormApp','ngAnimate','ui.bootstrap','ui.router','ngRoute','vAccordion','ngMaterial']);
